52567c2a7e886c0e54cbb02ee1740017425a7b89
Max1Denysov <72945727+Max1Denysov@users.noreply.github.com>
2020-11-22 01:53:21 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,5 @@
+ let number = ['  C  ','  A  ','  R  ','  C  ','   O   ','  Z  ', '  A  ' ,'N' ,'D','A'];
+let Bumber = [ '  P  ' , '  R  ' , '  E  ' , '  S  ' , '  S  '];
+document.write('<p id="jstext">' + number[0] + number[1] +  number[2] +  number[3] +  number[4] +  Bumber[4] +  number[1] +'</p>');
+ 
+document.write('<p id="jstexts">' + Bumber[0] +  Bumber[1]  + Bumber[2]  + Bumber[3]  + Bumber[4] +   '</p>');
